5851b61b5ed65671fc0aae56b68a31d3b9c6dfe5
Seanghay Yath <seanghay.dev@gmail.com>
2022-10-03 17:26:43 +0700

    chore: init

---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+/**
+ * @type {HTMLFormElement}
+ */
+let f = form;
+let _config = config();
+
+const $text = f['text'];
+const $result = f['result'];
+
+f.addEventListener('input', run)
+
+function config() {
+  const params = new URLSearchParams(new FormData(f));
+  return Object.fromEntries(params.entries())
+}
+
+
+function run() {
+  const currentConfig = config();
+  
+  if (currentConfig.mode !== _config.mode) {
+    let temp = $result.value;
+    $result.value = $text.value;
+    $text.value = temp;
+  }
+
+  _config = currentConfig;
+
+  try {
+    $result.value = base64[_config.mode]($text.value);
+  } catch(e) {
+    $result.value = 'ERR!'
+  }
+}
+
+
+run();
